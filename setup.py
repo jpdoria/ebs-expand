3fd3e1da94829ce05690124358b15eb2ba630dce
jpdoria <jp@lazyadm.in>
2017-02-13 13:03:24 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as file_requirements:
+    requirements = file_requirements.read().splitlines()
+
+setup(
+    name='ebs_expand',
+    version='1.0.0',
+    author='John Paul P. Doria',
+    author_email='jp@lazyadm.in',
+    description=('Expand root volume of EBS-backed Linux EC2.'),
+    license='MIT',
+    keywords='aws ec2 ebs expand',
+    url='https://github.com/jpdoria/ebs-expand',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'ebs_expand = ebs_expand.ebs_expand:main'
+        ]
+    },
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities'
+    ],
+)
